5cda12ac5a0975d071c7736285302251fb2efe7a
Asraful Islam <asraful009@gmail.com>
2026-06-13 16:32:39 +0600

    Initialize TinyQuest project structure and add essential files

---
+++ Source/TinyQuest/Private/TQMenuGameMode.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TQMenuGameMode.h"
+
+ATQMenuGameMode::ATQMenuGameMode()
+{
+    
+}
+
+void ATQMenuGameMode::BeginPlay()
+{
+    Super::BeginPlay();
+}

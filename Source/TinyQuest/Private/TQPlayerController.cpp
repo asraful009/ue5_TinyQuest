b946b8787ec71dd578254dbe08efc6f7c6c89e7d
Asraful Islam <asraful009@gmail.com>
2026-06-19 19:35:11 +0600

    Add Enhanced Input support to TQPlayerController; configure input actions and mapping context

--- Source/TinyQuest/Private/TQPlayerController.cpp
+++ Source/TinyQuest/Private/TQPlayerController.cpp
@@ -3,6 +3,49 @@
 
 #include "TQPlayerController.h"
 
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+
 ATQPlayerController::ATQPlayerController()
 {
+    bShowMouseCursor = false;
+}
+
+void ATQPlayerController::BeginPlay()
+{
+    Super::BeginPlay();
+}
+
+void ATQPlayerController::OnPossess(APawn* InPawn)
+{
+    Super::OnPossess(InPawn);
+    if (const TObjectPtr<ULocalPlayer> LocalPlayer = Cast<ULocalPlayer>(Player))
+    {
+        if (const TObjectPtr<UEnhancedInputLocalPlayerSubsystem> Subsystem 
+            = LocalPlayer->GetSubsystem<UEnhancedInputLocalPlayerSubsystem>())
+        {
+            if (Player3rdPersonIMC)
+            {
+                Subsystem->AddMappingContext(Player3rdPersonIMC, MappingPriority);
+            }
+        }
+    }
+}
+
+void ATQPlayerController::SetupInputComponent()
+{
+    Super::SetupInputComponent();
+    if (const TObjectPtr<UEnhancedInputComponent> EnhancedInputComponent = 
+        Cast<UEnhancedInputComponent>(InputComponent))
+    {
+        if (MoveAction)
+        {
+            EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ATQPlayerController::HandleMovement);
+        }
+    }
+}
+
+void ATQPlayerController::HandleMovement(const FInputActionValue& Value)
+{
+    FVector2D MovementVector = Value.Get<FVector2D>();
 }
--- Source/TinyQuest/Public/TQPlayerController.h
+++ Source/TinyQuest/Public/TQPlayerController.h
@@ -6,6 +6,9 @@
 #include "GameFramework/PlayerController.h"
 #include "TQPlayerController.generated.h"
 
+class UInputMappingContext;
+struct FInputActionValue;
+class UInputAction;
 /**
  * 
  */
@@ -13,7 +16,35 @@ UCLASS()
 class TINYQUEST_API ATQPlayerController : public APlayerController
 {
     GENERATED_BODY()
-    
+
+protected:
+    /** Input asset configurations mapped with modern UE5 TObjectPtr */
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "TQ|Input", meta = (AllowPrivateAccess = "true"))
+    TObjectPtr<UInputMappingContext> Player3rdPersonIMC;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "TQ|Input", meta = (AllowPrivateAccess = "true"))
+    TObjectPtr<UInputAction> MoveAction;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "TQ|Input", meta = (AllowPrivateAccess = "true"))
+    TObjectPtr<UInputAction> LookAction;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "TQ|Input", meta = (AllowPrivateAccess = "true"))
+    TObjectPtr<UInputAction> JumpAction;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "TQ|Input", meta = (AllowPrivateAccess = "true"))
+    int32 MappingPriority = 0;
+
 public:
     ATQPlayerController();
+
+protected:
+    virtual void BeginPlay() override;
+    virtual void OnPossess(APawn* InPawn) override;
+
+    virtual void SetupInputComponent() override;
+
+    UFUNCTION()
+    void HandleMovement(const FInputActionValue& Value);
+
+
 };

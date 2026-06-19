531bf6bffa2bd72f73764c49701cb53b6da15664
Asraful Islam <asraful009@gmail.com>
2026-06-19 18:34:21 +0600

    Add player character class with basic functionality and input setup

---
+++ Source/TinyQuest/Private/TQPlayerCharacter.cpp
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TQPlayerCharacter.h"
+
+// Sets default values
+ATQPlayerCharacter::ATQPlayerCharacter()
+{
+    // Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+    PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ATQPlayerCharacter::BeginPlay()
+{
+    Super::BeginPlay();
+}
+
+// Called every frame
+void ATQPlayerCharacter::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void ATQPlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+    Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
---
+++ Source/TinyQuest/Public/TQPlayerCharacter.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "TQPlayerCharacter.generated.h"
+
+UCLASS()
+class TINYQUEST_API ATQPlayerCharacter : public ACharacter
+{
+    GENERATED_BODY()
+
+public:
+    // Sets default values for this character's properties
+    ATQPlayerCharacter();
+
+protected:
+    // Called when the game starts or when spawned
+    virtual void BeginPlay() override;
+
+public:
+    // Called every frame
+    virtual void Tick(float DeltaTime) override;
+
+    // Called to bind functionality to input
+    virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+};
